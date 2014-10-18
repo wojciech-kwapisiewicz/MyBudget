89d2503573f58c8f576e89413c4c4c622172c628
kwapisiewicz <wojciech.kwapisiewicz@gmail.com>
2014-10-18 19:38:22 +0200

    https://github.com/kwapisiewicz/MyBudget/issues/3 Missing file checkin

---
+++ MyBudget.UI.Core/Converters/EmptyToDefaultTextConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MyBudget.UI.Core.Converters
+{
+    public class EmptyToDefaultTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if(value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return parameter ?? string.Empty;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
