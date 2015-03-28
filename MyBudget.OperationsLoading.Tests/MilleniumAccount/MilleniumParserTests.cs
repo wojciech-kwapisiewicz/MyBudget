749e30e24493821ed27144385d5f8252b617100d
kwapisiewicz <wojciech.kwapisiewicz@gmail.com>
2015-03-28 22:53:39 +0100

    https://github.com/kwapisiewicz/MyBudget/issues/16 Missing files

---
+++ MyBudget.OperationsLoading.Tests/MilleniumAccount/MilleniumParserTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using MyBudget.Core.DataContext;
+using MyBudget.Model;
+using MyBudget.OperationsLoading;
+using MyBudget.OperationsLoading.MilleniumAccount;
+using MyBudget.OperationsLoading.Tests.Resources;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBudget.Core.UnitTests.ImportData
+{
+    [TestFixture]
+    public class MilleniumParserTests
+    {
+        [Test]
+        public void GivenSampleMilleniumCsv_WhenParse_ThenListOfOperationsReturnedAnd2AccountsCreated()
+        {
+            //Given
+            Mock<IRepository<BankAccount, string>> accountRepo = new Mock<IRepository<BankAccount, string>>();
+            Mock<IRepository<BankOperationType, string>> typeRepo = new Mock<IRepository<BankOperationType, string>>();
+            string milleniumCsv = TestFiles.MilleniumParser_Sample;
+
+            //When
+            var list = new MilleniumParser(
+                new ParseHelper(accountRepo.Object, typeRepo.Object))
+                .Parse(milleniumCsv).ToArray();
+
+            //Then
+            Assert.AreEqual(7, list.Count());
+            //accountRepo.Verify(a => a.Add(It.IsAny<BankAccount>()), Times.Exactly(2));
+            //typeRepo.Verify(a => a.Add(It.IsAny<BankOperationType>()), Times.Exactly(6));
+        }
+    }
+}
---
+++ MyBudget.OperationsLoading.Tests/PkoBpCreditCard/PkoBpCreditCardParserTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using MyBudget.Core.DataContext;
+using MyBudget.Model;
+using MyBudget.OperationsLoading;
+using MyBudget.OperationsLoading.PkoBpCreditCard;
+using MyBudget.OperationsLoading.Tests.Resources;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBudget.Core.UnitTests.ImportData
+{
+    [TestFixture]
+    public class PkoBpCreditCardParserTests
+    {
+        [Test]
+        public void GivenSampleCreditCardText_WhenParse_ThenListOfOperationsReturnedAndAccountCreated()
+        {
+            //Given
+            Mock<IRepository<BankAccount, string>> accountRepo = new Mock<IRepository<BankAccount, string>>();
+            Mock<IRepository<BankOperationType, string>> typeRepo = new Mock<IRepository<BankOperationType, string>>();
+            string creditCardText = TestFiles.PkoBpCreditCardParser_Sample;
+
+            //When
+            var list = new PkoBpCreditCardUnclearedParser(
+                new ParseHelper(accountRepo.Object, typeRepo.Object),
+                new CreditCardTextParsing(),
+                new CreditCardUnclearedTextParsing())
+                .Parse(creditCardText).ToArray();
+
+            //Then
+            Assert.AreEqual(3, list.Count());
+            //accountRepo.Verify(a => a.Add(It.IsAny<BankAccount>()), Times.Exactly(1));
+            //typeRepo.Verify(a => a.Add(It.IsAny<BankOperationType>()), Times.Exactly(2));
+        }
+    }
+}
