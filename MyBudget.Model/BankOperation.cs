9f37fd06fd4270d44f31b259c36560be8a019e95
kwapisiewicz <wojciech.kwapisiewicz@gmail.com>
2015-04-25 15:51:57 +0200

    https://github.com/kwapisiewicz/MyBudget/issues/18 Fixed grid not beeing updated when Category/SubCategory changes

--- MyBudget.Model/BankOperation.cs
+++ MyBudget.Model/BankOperation.cs
@@ -1,6 +1,7 @@
 ﻿using MyBudget.Core.DataContext;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace MyBudget.Model
 {
-    public class BankOperation : IIdentifiable<int>
+    public class BankOperation : NotifiableObject, IIdentifiable<int>
     {
         public int Id { get; set; }
         public BankAccount BankAccount { get; set; }
@@ -23,11 +24,37 @@ namespace MyBudget.Model
         public decimal EndingBalance { get; set; }
         public string Description { get; set; }
 
-        public string Category { get; set; }
-        public string SubCategory { get; set; }
+        private string _category;
+        public string Category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                _category = value;
+                OnPropertyChanged(() => Category);
+            }
+        }
+        
+        private string _subcategory;
+        public string SubCategory
+        {
+            get
+            {
+                return _subcategory;
+            }
+            set
+            {
+                _subcategory = value;
+                OnPropertyChanged(() => SubCategory);
+            }
+        }
+
         public Card Card { get; set; }
 
         public string Title { get; set; }
-        public string CounterAccount { get; set; }
+        public string CounterAccount { get; set; }   
     }
 }
---
+++ MyBudget.Model/NotifiableObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBudget.Model
+{
+    public abstract class NotifiableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void OnPropertyChanged<T>(Expression<Func<T>> selectorExpression)
+        {
+            MemberExpression body = selectorExpression.Body as MemberExpression;
+            if (body == null) throw new ArgumentException("The body must be a member expression");
+
+            PropertyChanged(this, new PropertyChangedEventArgs(body.Member.Name)); 
+        }
+    }
+}
