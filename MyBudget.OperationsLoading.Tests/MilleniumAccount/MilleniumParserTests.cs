76cc7c930f9e3ccf9fca7d7d1c0e9c9fa2214da6
gniady <gniady@GNIADY-PC>
2020-02-18 19:51:31 +0100

    https://github.com/kwapisiewicz/MyBudget/issues/23 Fixed tests and testcases to be realistic and to check all transactions.

--- MyBudget.OperationsLoading.Tests/MilleniumAccount/MilleniumParserTests.cs
+++ MyBudget.OperationsLoading.Tests/MilleniumAccount/MilleniumParserTests.cs
@@ -21,6 +21,8 @@ namespace MyBudget.OperationsLoading.Tests.MilleniumAccount
         private Mock<IRepository<Card, string>> cardRepo;
         private MilleniumParser parser;
 
+        private List<BankAccount> mockAccountsCreated = new List<BankAccount>();
+
         [SetUp]
         public void SetUp()
         {
@@ -28,6 +30,11 @@ namespace MyBudget.OperationsLoading.Tests.MilleniumAccount
             this.typeRepo = new Mock<IRepository<BankOperationType, string>>();
             this.cardRepo = new Mock<IRepository<Card, string>>();
             this.parser = new MilleniumParser(new ParseHelper(), new RepositoryHelper(accountRepo.Object, typeRepo.Object, cardRepo.Object));
+
+            this.accountRepo.Setup(a => a.Get(It.IsAny<string>())).Returns<string>(
+                a => mockAccountsCreated.FirstOrDefault(x => x.Number == a));
+            this.accountRepo.Setup(a => a.Add(It.IsAny<BankAccount>())).Callback<BankAccount>(
+                a => mockAccountsCreated.Add(a));
         }
 
         [Test]
@@ -44,12 +51,140 @@ namespace MyBudget.OperationsLoading.Tests.MilleniumAccount
 
         private static void VerifyOperations(IEnumerable<BankOperation> operations)
         {
-            //Assert.Inconclusive("Implement checks of operations");
+            #region Account no 1
+
+            Assert.IsTrue(operations.Any(op =>
+                op.BankAccount.Number == TestBankData.Millenium_TestAccount1.Compact() &&
+                op.ExecutionDate == new DateTime(2014, 09, 20) &&
+                op.OrderDate == new DateTime(2014, 09, 20) &&
+                op.Amount == -1234.00M &&
+                op.Type.Name == "PRZELEW WEWNĘTRZNY WYCHODZĄCY" &&
+                op.Cleared == true &&
+                op.Description == "Przelew własny" &&
+                op.Title == "Przelew własny".GetFirstNCharacters(OperationsLoadingConsts.OperationTitleLength) &&
+                op.EndingBalance == 933.89M &&
+                op.Card == null &&
+                string.IsNullOrEmpty(op.CounterAccount)));
+
+            Assert.IsTrue(operations.Any(op =>
+                op.BankAccount.Number == TestBankData.Millenium_TestAccount1.Compact() &&
+                op.ExecutionDate == new DateTime(2014, 09, 19) &&
+                op.OrderDate == new DateTime(2014, 09, 19) &&
+                op.Amount == -11.22M &&
+                op.Type.Name == "OBCIĄŻENIE" &&
+                op.Cleared == true &&
+                op.Description == "PODATEK OD ODSETEK" &&
+                op.Title == "PODATEK OD ODSETEK".GetFirstNCharacters(OperationsLoadingConsts.OperationTitleLength) &&
+                op.EndingBalance == 2167.89M &&
+                op.Card == null &&
+                string.IsNullOrEmpty(op.CounterAccount)));
+
+            Assert.IsTrue(operations.Any(op =>
+                op.BankAccount.Number == TestBankData.Millenium_TestAccount1.Compact() &&
+                op.ExecutionDate == new DateTime(2014, 09, 18) &&
+                op.OrderDate == new DateTime(2014, 09, 18) &&
+                op.Amount == 55.66M &&
+                op.Type.Name == "UZNANIE" &&
+                op.Cleared == true &&
+                op.Description == "KAPITALIZACJA ODS." &&
+                op.Title == "KAPITALIZACJA ODS.".GetFirstNCharacters(OperationsLoadingConsts.OperationTitleLength) &&
+                op.EndingBalance == 2179.11M &&
+                op.Card == null &&
+                string.IsNullOrEmpty(op.CounterAccount)));
+
+            Assert.IsTrue(operations.Any(op =>
+                op.BankAccount.Number == TestBankData.Millenium_TestAccount1.Compact() &&
+                op.ExecutionDate == new DateTime(2014, 09, 17) &&
+                op.OrderDate == new DateTime(2014, 09, 17) &&
+                op.Amount == 123.45M &&
+                op.Type.Name == "PRZELEW PRZYCHODZĄCY" &&
+                op.Cleared == true &&
+                op.Description == "Bardzo dlugi tytul na ponad 30 znakow ktore trzeba bedzie przyciac" &&
+                op.Title == "Bardzo dlugi tytul na ponad 30 znakow ktore trzeba bedzie przyciac".GetFirstNCharacters(OperationsLoadingConsts.OperationTitleLength) &&
+                op.EndingBalance == 2123.45M &&
+                op.Card == null &&
+                op.CounterAccount == "11 22 2233 3344 4455 5566 6677 77".Compact()));
+
+            #endregion
+
+            #region Account no 2
+
+            Assert.IsTrue(operations.Any(op =>
+                op.BankAccount.Number == TestBankData.Millenium_TestAccount2.Compact() &&
+                op.ExecutionDate == new DateTime(2014, 09, 16) &&
+                op.OrderDate == new DateTime(2014, 09, 14) &&
+                op.Amount == -3.00M &&
+                op.Type.Name == "TRANSAKCJA KARTĄ PŁATNICZĄ" &&
+                op.Cleared == true &&
+                op.Description == "Trans" &&
+                op.Title == "Trans".GetFirstNCharacters(OperationsLoadingConsts.OperationTitleLength) &&
+                op.EndingBalance == 1990.37M &&
+                op.Card == null &&
+                string.IsNullOrEmpty(op.CounterAccount)));
+
+            Assert.IsTrue(operations.Any(op =>
+                op.BankAccount.Number == TestBankData.Millenium_TestAccount2.Compact() &&
+                op.ExecutionDate == new DateTime(2014, 09, 13) &&
+                op.OrderDate == new DateTime(2014, 09, 13) &&
+                op.Amount == -3.52M &&
+                op.Type.Name == "WYPŁATA KARTĄ Z BANKOMATU" &&
+                op.Cleared == true &&
+                op.Description == "xx1" &&
+                op.Title == "xx1".GetFirstNCharacters(OperationsLoadingConsts.OperationTitleLength) &&
+                op.EndingBalance == 1993.37M &&
+                op.Card == null &&
+                string.IsNullOrEmpty(op.CounterAccount)));
+
+            Assert.IsTrue(operations.Any(op =>
+                op.BankAccount.Number == TestBankData.Millenium_TestAccount2.Compact() &&
+                op.ExecutionDate == new DateTime(2014, 09, 12) &&
+                op.OrderDate == new DateTime(2014, 09, 12) &&
+                op.Amount == -3.11M &&
+                op.Type.Name == "WYPŁATA KARTĄ Z BANKOMATU" &&
+                op.Cleared == true &&
+                op.Description == "xx1" &&
+                op.Title == "xx1".GetFirstNCharacters(OperationsLoadingConsts.OperationTitleLength) &&
+                op.EndingBalance == 1996.89M &&
+                op.Card == null &&
+                string.IsNullOrEmpty(op.CounterAccount)));
+
+            #endregion
+
         }
 
-        private static void VerifyAccountAndCardNumbersAndOperationTypes(IEnumerable<BankOperation> operations)
+        private void VerifyAccountAndCardNumbersAndOperationTypes(IEnumerable<BankOperation> operations)
         {
-            //Assert.Inconclusive("Implement checks of operations");        
+            //We want just 1 account of each type to be inserted
+            accountRepo.Verify(repo => repo.Add(
+                It.Is<BankAccount>(account =>
+                account.Number == TestBankData.Millenium_TestAccount1.Compact())), Times.Once);
+            accountRepo.Verify(repo => repo.Get(
+                It.Is<string>(accountNumber => accountNumber == TestBankData.Millenium_TestAccount1.Compact())), Times.Exactly(4));
+
+            accountRepo.Verify(repo => repo.Add(
+                It.Is<BankAccount>(account =>
+                account.Number == TestBankData.Millenium_TestAccount2.Compact())), Times.Once);
+            accountRepo.Verify(repo => repo.Get(
+                It.Is<string>(accountNumber => accountNumber == TestBankData.Millenium_TestAccount2.Compact())), Times.Exactly(3));
+
+            typeRepo.Verify(repo => repo.Add(
+                It.Is<BankOperationType>(type =>
+                type.Name == "PRZELEW WEWNĘTRZNY WYCHODZĄCY")));
+            typeRepo.Verify(repo => repo.Add(
+                It.Is<BankOperationType>(type =>
+                type.Name == "OBCIĄŻENIE")));
+            typeRepo.Verify(repo => repo.Add(
+                It.Is<BankOperationType>(type =>
+                type.Name == "UZNANIE")));
+            typeRepo.Verify(repo => repo.Add(
+                It.Is<BankOperationType>(type =>
+                type.Name == "PRZELEW PRZYCHODZĄCY")));
+            typeRepo.Verify(repo => repo.Add(
+                It.Is<BankOperationType>(type =>
+                type.Name == "TRANSAKCJA KARTĄ PŁATNICZĄ")));
+            typeRepo.Verify(repo => repo.Add(
+                It.Is<BankOperationType>(type =>
+                type.Name == "WYPŁATA KARTĄ Z BANKOMATU")));
         }
 
         [Test]
