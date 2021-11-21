e6bc3358886f39d89c7f7bec590ce33901e8831e
Wojciech Kwapisiewicz <wojciech.kwapisiewicz@gmail.com>
2021-11-21 01:16:39 +0100

    https://github.com/kwapisiewicz/MyBudget/issues/31 Add UI part

--- MyBudget.Model/ClassificationRule.cs
+++ MyBudget.Model/ClassificationRule.cs
@@ -10,6 +10,7 @@ namespace MyBudget.Model
     public class ClassificationRule : IIdentifiable<int>
     {
         public const string SavedAccount = "SavedAccount";
+        [System.Xml.Serialization.XmlIgnore]
         public readonly List<string> SearchedFieldNames = new List<string>() { "Description", "Title", "CounterParty" };
         public const RuleType Type = RuleType.Mixed;
         
