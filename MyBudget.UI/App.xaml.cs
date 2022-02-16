6ca797e448c61ae6c2583212c5c5055fcb93c01f
Wojciech Kwapisiewicz <wojciech.kwapisiewicz@gmail.com>
2022-02-16 12:54:41 +0100

    https://github.com/kwapisiewicz/MyBudget/issues/30 Handling of null description Increased version on the main view bar Added searching by operation type

--- MyBudget.UI/App.xaml.cs
+++ MyBudget.UI/App.xaml.cs
@@ -15,8 +15,23 @@ namespace MyBudget.UI
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
             new Bootstrapper().Run();
             base.OnStartup(e);
+        }
+
+        private void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+
+            string errorMessage = string.Format("An application error occurred.\nPlease check whether your data is correct and repeat the action. If this error occurs again there seems to be a more serious malfunction in the application, and you better close it.\n\nError: {0}\n\nDo you want to continue?\n(if you click Yes you will continue with your work, if you click No the application will close)",
+                e.Exception.Message + (e.Exception.InnerException != null ? "\n" +
+                e.Exception.InnerException.Message : null));
+
+            if (MessageBox.Show(errorMessage, "Application Error", MessageBoxButton.YesNoCancel, MessageBoxImage.Error) == MessageBoxResult.No)
+            {
+                Current.Shutdown();
+            }
         }
     }
 }
